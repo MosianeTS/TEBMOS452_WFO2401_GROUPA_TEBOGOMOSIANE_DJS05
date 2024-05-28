d443fa24f45872b34f0f190cb8cb23565cb3a57d
Tebogo Mosiane <mosianets@gmail.com>
2024-05-28 16:17:39 +0200

    Updated UI to enable user interaction

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-// Action creators
 
-const add = () => ({ type: "ADD" });
-const subtract = () => ({ type: "SUBTRACT" });
-const reset = () => ({ type: "RESET" });
+const initialState = { value: 0 };
 
-// Initial state
-const initialState = { count: 0 };
-
-// Reducer
 const reducer = (state = initialState, action) => {
   switch (action.type) {   
     case "ADD":
-      return { ...state, count: state.count + 1 };
+      return { ...state, value: state.value + 1 };
     case "SUBTRACT":
-      return { ...state, count: state.count - 1 };
+      return { ...state, value: state.value - 1 };
     case "RESET":
-      return { ...state, count: 0 };
+      return { ...state, value: 0 };
     default:
       return state;
   }
 };
 
-// Create store function
 const createStore = (reducer) => {
   let state;
   let listeners = [];
@@ -46,22 +38,23 @@ const createStore = (reducer) => {
   return { getState, dispatch, subscribe };
 };
 
+
+
 // Create the store
 const store = createStore(reducer);
 
 // Logging the initial state
-console.log("Initial state:", store.getState().count);
+console.log("Initial state:", store.getState().value);
 
 // Subscribe to store updates
 const unsubscribe = store.subscribe(() => {
-  console.log("State changed:", store.getState().count);
+  console.log("State changed:", store.getState().value);
+  document.getElementById('display').textContent = store.getState().value
 });
 
 // Dispatching actions
-store.dispatch(add());
-store.dispatch(add());
-store.dispatch(subtract());
-store.dispatch(reset());
+document.getElementById('add').addEventListener('click', () => {store.dispatch({ type: "ADD" })});
+document.getElementById('subtract').addEventListener('click', () => {store.dispatch({ type: "SUBTRACT" }); console.log("Yeah")});
+document.getElementById('reset').addEventListener('click', () => store.dispatch({ type: "RESET" }));
 
-// Unsubscribe from store updates
-unsubscribe();
+
